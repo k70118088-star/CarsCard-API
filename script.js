5e28c34ad02beb609b4d8339f785a728fb566739
k70118088-star <k70118088@gmail.com>
2025-12-30 16:04:42 +0530

    car cards API

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.getElementById("cars");
+
+fetch("https://dummyjson.com/c/68a9-8ef6-4b20-a51c")
+  .then(res => res.json())
+  .then(data => {
+    const cars = data.cars ?? data; 
+
+    cars.forEach(car => {
+      const card = document.createElement("div");
+      card.className = "car-card";
+
+      card.innerHTML = `
+        <img src="${car.image}" alt="${car.name}">
+        <div class="car-info">
+          <h3>${car.name}</h3>
+          <p class="brand">${car.brand}</p>
+          <p class="rating">⭐ ${car.rating}</p>
+          <p class="price">${car.price}</p>
+        </div>
+      `;
+
+      container.appendChild(card);
+    });
+  })
+  .catch(err => {
+    console.error("Fetch error:", err);
+    container.innerHTML = "<p>Unable to load cars 😕</p>";
+  });
